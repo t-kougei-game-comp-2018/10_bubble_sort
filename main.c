ef4a4cbcda84894bc065b768be259bb3de886b45
mochizukidaiki <43572081+mochizukidaiki@users.noreply.github.com>
2018-12-10 01:01:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[7];
+    int str[7];
     
    int i;
 
-  for (i = 0; i < 7; i++) {
+  for (i = 0; i <= 100; i++) {
    
       if (str[i] > str[i+1]) {
        str[i]=str[i+1]
