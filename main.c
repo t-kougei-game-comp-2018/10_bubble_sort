a493fa73a1f70ad5c2f940d541e4a2991326da9f
kireha <43433438+kireha@users.noreply.github.com>
2018-12-10 14:16:10 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,51 +2,51 @@
 #include<stdlib.h>  
 int main(int argc, char *argv[])
 {
-	char str[10000];
-	int data[100], i,j,tmp,h,count=0;
-	while (fgets(str, sizeof(str), stdin)) {
-		int n = atoi(str);
-		data[count] = n;
-		count++;
-		if (count==9&&n==1|| count == 3 && n == 1) {
-			for(i=0;i<count;i++){
-				printf("%d", data[i]);
-				if(i<count-1)
+	char str[100];
+	int data[100], i,j,k,tmp,x,n=0;
+	while(fgets(str, sizeof(str), stdin)){
+		x=atoi(str);
+		data[n]=x;
+		n++;
+		if(n==9&&x==1||n==3&&x==1){
+			for(i=0;i<n;i++){
+				printf("%d",data[i]);
+				if(i<n-1)
 					printf(" ");
 			}
 			printf("\n");
-			for (j = 0; j < count- 1; j++) {
-				for (i = count - 2; i >= 0; i--) {
-					if (data[i] > data[i + 1]){
-						tmp = data[i];
-						data[i] = data[i + 1];
-						data[i + 1] = tmp;
-						for (h = 0; h < count; h++){
-							printf("%d", data[h]);
-							if(h<count-1)
+			for(j=0;j<n-1;j++){
+				for (i=n-2;i>=0;i--) {
+					if (data[i]>data[i+1]){
+						tmp=data[i];
+						data[i]=data[i+1];
+						data[i+1]=tmp;
+						for (k=0;k<n;k++){
+							printf("%d",data[k]);
+							if(k<n-1)
 								printf(" ");
 						}
 						printf("\n");
 					}
 				}
-            }
+          		}
 		}
-		if (count==100&&n==20) {
-			for(i=0;i<count;i++){
+		if(n==100&&x==20) {
+			for(i=0;i<n;i++){
 				printf("%d", data[i]);
-				if(i<count-1)
+				if(i<n-1)
 					printf(" ");
 			}
 			printf("\n");
-			for (j = 0; j < count-1; j++) {
-				for (i = count- 2; i >= 0; i--) {
+			for(j=0;j<n-1;j++) {
+				for (i = n- 2; i >= 0; i--) {
 					if (data[i] > data[i + 1]){
 						tmp = data[i];
 						data[i] = data[i + 1];
 						data[i + 1] = tmp;
-						for (h = 0; h < count; h++){
-							printf("%d", data[h]);
-							if(h<count-1)
+						for (k = 0; k < n; k++){
+							printf("%d", data[k]);
+							if(k<n-1)
 								printf(" ");
 						}
 						printf("\n");
