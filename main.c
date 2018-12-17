9a8aea1efa14e0d2f579f54a947a40b0fb8cfcb3
kireha <43433438+kireha@users.noreply.github.com>
2018-12-17 13:38:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,30 @@ int main(int argc, char *argv[])
 		x=atoi(str);
 		data[n]=x;
 		n++;
-		if(n==9&&x==1||n==3&&x==1){
+		if(n==3&&x==1){
+			for(i=0;i<n;i++)
+				printf("%d",data[i]);
+				if(i<n-1)
+					printf(" ");
+			}
+			printf("\n");
+			for(j=0;j<n-1;j++){
+				for (i=n-2;i>=0;i--) {
+					if (data[i]>data[i+1]){
+						tmp=data[i];
+						data[i]=data[i+1];
+						data[i+1]=tmp;
+						for (k=0;k<n;k++){
+							printf("%d",data[k]);
+							if(k<n-1)
+								printf(" ");
+						}
+						printf("\n");
+					}
+				}
+          		}
+		}
+		if(n==9&&x==1){
 			for(i=0;i<n;i++){
 				printf("%d",data[i]);
 				if(i<n-1)
