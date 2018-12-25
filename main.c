740d93c008a6b9477c7034bcba41e3a1069d68fe
momomizukubota <momomizukubota>
2018-12-26 01:46:50 +0900

    01

--- main.c
+++ main.c
@@ -1,12 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) 
+void print(int *data, int count)
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	for (int k = 0; k < count; k++)
+	{
+		if (k == 0)
+		{
+			printf("%d", data[k]);
+		}
+		else
+		{
+			printf(" %d", data[k]);
+		}
+	}
+	printf("\n");
+}
 
-    return 0;
+int main(int argc, char *argv[])
+{
+	char str[4];
+	int tmp[100];
+	int count = 0;
+
+	while (fgets(str, sizeof(str), stdin)) {
+		int number = atoi(str);
+
+		tmp[count++] = number;
+	}
+
+	int *data = (int *)malloc(sizeof(int) * count);
+	memcpy(data, tmp, sizeof(int) * count);
+
+	print(data, count);
+
+	for (int i = 0; i < count - 1; i++)
+	{
+		for (int j = count - 1; j > i; j--)
+		{
+			if (data[j - 1] > data[j])
+			{
+				int tm = data[j - 1];
+				data[j - 1] = data[j];
+				data[j] = tm;
+
+				print(data, count);
+			}
+		}
+	}
+
+	free(data);
+	return 0;
 }
